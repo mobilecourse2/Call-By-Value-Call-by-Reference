07da8ce8594c2ec0a58226daacd34c78eb4de740
Mobile Application Course <44363418+mobilecourse2@users.noreply.github.com>
2018-10-31 10:14:24 +0500

    Add files via upload

---
+++ Call&reference.java
@@ -0,0 +1,39 @@
+package stringfunctions;
+
+public class StringFunctions {
+
+    public static void main(String[] args) {
+   
+        
+        int[] value = {155,145};
+        System.out.println("Before caling ptintArray method vlaue[0] =" + value[0]);
+        pritnArray(value);
+        System.out.println("After caling ptintArray method vlaue[0] =" + value[0]);
+        /*     String s1="  hello   ";  
+         System.out.println(s1+"how are you");        // without trim()  
+         System.out.println(s1.trim()+"how are you"); // with trim() */
+        /*int value=20; 
+         String s1=String.valueOf(value); 
+         System.out.println(s1+17);
+         */
+        /*   int number = 25;
+        System.out.println("Before caling display method number=" + number);
+        display(number);
+        System.out.println("After caling display method number=" + number);*/
+     
+
+        
+    }
+    /*
+     public static void display(int num) {
+        System.out.println("inside display method value of num" + num);
+        num = 100;
+        System.out.println("inside display method value of num" + num);
+    }*/
+   
+    public static void pritnArray(int[] array) {
+        System.out.println("Inside  ptintArray method vlaue[0] =" + array[0]);
+        array[0] = 100;
+        System.out.println("Inside  ptintArray method after changing vlaue[0] =" + array[0]);
+    }
+}
